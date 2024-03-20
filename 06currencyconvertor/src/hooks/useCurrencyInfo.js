b3d0988369348c46cc4afef37ce09e5c1267cf2e
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-03-20 22:25:19 +0530

    currency convertor completed

--- 06currencyconvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyconvertor/src/hooks/useCurrencyInfo.js
@@ -4,11 +4,12 @@ function useCurrencyInfo(currency) {
   const [data, setData] = useState({});
   useEffect(() => {
     fetch(
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.min.json`
     )
       .then((res) => res.json())
       .then((res) => setData(res[currency]));
   }, [currency]);
+  console.log(data);
   return data;
 }
 
